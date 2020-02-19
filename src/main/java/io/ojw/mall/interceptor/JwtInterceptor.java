d7cc32b3829289295a1a4c015d99f4647670c8f6
ojwman <ohjae8851@naver.com>
2020-02-19 17:07:55 +0900

    add logback

--- src/main/java/io/ojw/mall/interceptor/JwtInterceptor.java
+++ src/main/java/io/ojw/mall/interceptor/JwtInterceptor.java
@@ -9,8 +9,13 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import io.ojw.mall.user.jwt.JwtService;
 import io.ojw.mall.user.jwt.UnauthorizedException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 public class JwtInterceptor extends HandlerInterceptorAdapter {
+	Logger logger = LoggerFactory.getLogger("io.ojw.mall.interceptor.JwtInterceptor");
+	
 	private static final String TOKEN = "token";
 
 	private JwtService jwtService;
@@ -25,6 +30,11 @@ public class JwtInterceptor extends HandlerInterceptorAdapter {
 		final String token = request.getHeader(TOKEN);
 		
 		System.out.println("JwtInterceptor > preHandle > token: " + token);
+	    logger.trace("Hello world.");
+	    logger.debug("Hello world.");
+//	    logger.info("Hello world.");
+//	    logger.warn("Hello world.");
+//	    logger.error("Hello world.");
 		
 		if(token != null && jwtService.checkToken(token)){
 			return true;
