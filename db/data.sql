002c586e86e1fce54a966b07376158a4f1f6e41e
leesomi <122369@naver.com>
2020-02-22 14:18:54 +0900

    change db name

--- db/data.sql
+++ db/data.sql
@@ -1,3 +1,3 @@
-insert into user(id, name, password, auth, phone) values ('admin', '관리자', '1234', 'manager', '01041169771');
-insert into user(id, name, password, auth, phone) values ('ojw', '오재원', '1234', 'member', '01041169771');
+insert into user(id, name, password, auth, phone) values ('admin', 'admin', '1234', 'manager', '01041169771');
+insert into user(id, name, password, auth, phone) values ('ojw', 'ojw', '1234', 'member', '01041169771');
 
