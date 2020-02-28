480ad7ae5db20b25711245a60c55dd972860528c
ojwman <ohjae8851@naver.com>
2020-02-28 13:08:13 +0900

    add booting shell

---
+++ springboot-server-start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "start spring boot server"
+
+# remove
+rm -r sbp-shopping-mall
+
+# clone git
+git clone https://github.com/ojwman/sbp-shopping-mall
+
+# move
+cd sbp-shopping-mall
+
+# build
+mvn package
+
+# run
+cd target
+java -jar sbp-shopping-mall-0.0.1-SNAPSHOT.jar
+
+echo "boot"
