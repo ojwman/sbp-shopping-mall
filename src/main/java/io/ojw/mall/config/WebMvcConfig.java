03f5f82ebe22aa4d22baf659743b6780819913ee
ojwman <ohjae8851@naver.com>
2020-02-28 13:01:25 +0900

    allow 'http://35.222.169.33:9000'

--- src/main/java/io/ojw/mall/config/WebMvcConfig.java
+++ src/main/java/io/ojw/mall/config/WebMvcConfig.java
@@ -23,7 +23,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
     	registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://35.222.169.33:9000");
 
     }
 }
