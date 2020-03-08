920be0e2df8fa7973a45dd864205e98cba10903e
ojwman <ohjae8851@naver.com>
2020-03-08 12:02:48 +0900

    db script modified

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,17 @@
 drop table user if exists;
 create table user (
   id varchar(20) primary key,
-  name varchar(100),
-  password varchar(100),
+  name varchar(100) not null,
+  password varchar(100) not null,
   auth varchar(20),
   phone varchar(11)
+);
+
+drop table item if exists;
+create table item (
+	id bigint primary key auto_increment,
+	name varchar(100) not null,
+	category varchar(100) not null,
+	price int,
+	thumbnail clob
 );
