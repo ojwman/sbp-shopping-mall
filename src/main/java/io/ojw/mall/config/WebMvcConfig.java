0d3d272fbacf6c497249b6754c1726e1d571d841
ojwman <ohjae8851@naver.com>
2020-03-08 15:31:09 +0900

    [modify] allow client can read header("jwt-token")

--- src/main/java/io/ojw/mall/config/WebMvcConfig.java
+++ src/main/java/io/ojw/mall/config/WebMvcConfig.java
@@ -24,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
     	registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://35.222.169.33:9000")
-                .exposedHeaders("jwt-token").allowCredentials(false)	//allow client can read header('jwt-token')
+                .exposedHeaders("jwt-token")	//allow client can read header("jwt-token")
                 ;
 
     }
