99b8f918fc8fdb760762be7193b4c325d37fb355
ojwman <ohjae8851@naver.com>
2020-03-08 09:58:16 +0900

    @ResponseStatus(HttpStatus.UNAUTHORIZED)

--- src/main/java/io/ojw/mall/user/jwt/UnauthorizedException.java
+++ src/main/java/io/ojw/mall/user/jwt/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package io.ojw.mall.user.jwt;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends RuntimeException{
 	/**/
 	private static final long serialVersionUID = 1L;
