2d19e41c72cd93c41e5fd8fd1a53f850397439fa
ojwman <ohjae8851@naver.com>
2020-03-12 13:52:08 +0900

    schema change .. item

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 drop table user if exists;
 create table user (
-  id varchar(20) primary key,
-  name varchar(100) not null,
-  password varchar(100) not null,
-  auth varchar(20),
-  phone varchar(11)
+	id varchar(20) primary key,
+	name varchar(100) not null,
+	password varchar(100) not null,
+	auth varchar(20),
+	phone varchar(11)
 );
 
 drop table item if exists;
@@ -13,5 +13,6 @@ create table item (
 	name varchar(100) not null,
 	category varchar(100) not null,
 	price int,
-	thumbnail clob
+	thumbnail clob,
+	hashtag varchar
 );
